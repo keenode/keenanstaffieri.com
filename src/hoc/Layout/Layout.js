8196ac12205a924fd4e73cc4bd868f10c4b76c2b
keenode <kstaffieri@gmail.com>
2018-07-17 21:44:23 -0700

    adding social media icons

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,9 +1,15 @@
+// Social media icons from:
+// https://github.com/simple-icons/simple-icons/tree/develop/icons
+
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 import SVGInline from 'react-svg-inline'
 
 import Nav from '../../components/UI/Nav/Nav'
+
 import ksLogo from '../../assets/svgs/ks-logo.svg'
+import githubIcon from '../../assets/svgs/github.svg'
+import linkedinIcon from '../../assets/svgs/linkedin.svg'
 
 import styles from './Layout.css';
 
@@ -23,6 +29,14 @@ class Layout extends Component {
           {this.props.children}
         </main>
         <footer className={styles.footer}>
+          <div className={styles.socialMediaContainer}>
+            <a href="https://github.com/keenode" target="_blank" rel="noopener noreferrer">
+              <SVGInline className={styles.socialMediaIcon} svg={githubIcon} />
+            </a>
+            <a href="https://www.linkedin.com/in/kstaffieri" target="_blank" rel="noopener noreferrer">
+              <SVGInline className={styles.socialMediaIcon} svg={linkedinIcon} />
+            </a>
+          </div>
           <small>&copy; Copyright 2018 Keenan Staffieri.</small>
         </footer>
       </div>
