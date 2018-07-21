2e8c3c8bb7c29d4204f2e1b74c1f82c049041f7c
keenan-envoy <keenan@weareenvoy.com>
2018-07-20 19:28:07 -0700

    working on nav and borders

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -6,6 +6,12 @@ import styles from './Home.css'
 
 const home = () => (
   <Fragment>
+    <div className={styles.pageBorders}>
+      <span className={styles.pageBorder_Top}></span>
+      <span className={styles.pageBorder_Right}></span>
+      <span className={styles.pageBorder_Bottom}></span>
+      <span className={styles.pageBorder_Left}></span>
+    </div>
     <div className={styles.pageContainer}>
       <h1>Keenan Staffieri</h1>
       <h3>Frontend Developer</h3>
