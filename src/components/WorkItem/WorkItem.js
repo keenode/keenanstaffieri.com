11276422aca931dab26b7e1ec3f54e3d30dbb573
Keenan Staffieri <kstaffieri@gmail.com>
2018-07-24 22:03:33 -0700

    messing with scroll transitions on work

--- src/components/WorkItem/WorkItem.js
+++ src/components/WorkItem/WorkItem.js
@@ -6,33 +6,29 @@ import styles from './WorkItem.css'
 
 const workItem = props => (
   <article className={[styles.workItem, props.alignLeft ? styles.workItem_left : null].join(' ')}>
-    <ScrollAnimation animateIn={styles.fadeInMe}>
+    <ScrollAnimation animateIn={styles.fadeMoveUp} animateOnce>
       <Parallax
-          className="custom-class"
           offsetYMax={20}
           offsetYMin={-20}
           slowerScrollRate
-          tag="figure"
       >
         <div className={styles.workPicture}></div>
       </Parallax>
+      <Parallax
+          offsetYMax={20}
+          offsetYMin={-20}
+          slowerScrollRate
+      >
+        <div className={styles.workBox}>
+          <h3 className={styles.workBox_Heading}>{props.title}</h3>
+          {props.buttons.map((button, index) => (
+            <a key={index} className={styles.button} href={button.url} target="_blank" rel="noopener noreferrer">
+              {button.text}
+            </a>
+          ))}
+        </div>
+      </Parallax>
     </ScrollAnimation>
-    <Parallax
-        className="custom-class"
-        offsetYMax={20}
-        offsetYMin={-20}
-        slowerScrollRate
-        tag="figure"
-    >
-      <div className={styles.workBox}>
-        <h3 className={styles.workBox_Heading}>{props.title}</h3>
-        {props.buttons.map((button, index) => (
-          <a key={index} className={styles.button} href={button.url} target="_blank" rel="noopener noreferrer">
-            {button.text}
-          </a>
-        ))}
-      </div>
-    </Parallax>
   </article>
 )
 
