18b5ae6a24ff4eff79e02bc16a829eb75c550658
keenode <kstaffieri@gmail.com>
2018-07-26 20:39:55 -0700

    working on about styles

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -7,18 +7,16 @@ import styles from './About.css'
 const about = () => (
   <div className={styles.pageContainer}>
     <HeadingCanvas text="About Me" />
-    <article className={styles.story}>
-      <p>My name is Keenan Staffieri. I am passionate about...</p>
-      <p>Velit fugiat duis sit fugiat aute velit sit enim aliquip do est duis quis.</p>
-      <p>Exercitation irure non sint velit mollit Lorem proident culpa duis labore. Id incididunt cillum laborum veniam exercitation est officia. Minim culpa voluptate consectetur labore et proident aute anim commodo laborum non ut. Occaecat adipisicing aliqua culpa tempor laborum ex cillum proident aute sint. Aliqua est deserunt anim incididunt nisi laborum amet consectetur labore fugiat mollit. Id reprehenderit exercitation id dolore velit.</p>
-      <p>Eiusmod sint ad dolore est. Irure elit officia culpa officia. Exercitation ut ullamco eiusmod laborum cillum ut incididunt. Pariatur occaecat nulla veniam est elit. Est deserunt ea officia est ea in voluptate in nostrud veniam. Elit ex ea amet ex duis enim nostrud laborum aliquip. Laborum sit eu nostrud occaecat do fugiat velit reprehenderit dolor.</p>
-      <p>Dolore irure culpa sint voluptate ullamco irure. Dolor adipisicing nisi aliquip laboris anim aliqua. Aute amet consequat sint veniam incididunt reprehenderit occaecat proident fugiat laborum. Pariatur sit officia nisi proident culpa. Reprehenderit consectetur magna qui qui consectetur laborum ut.</p>
-    </article>
-    <h2>Skills</h2>
-    <ul>
-      <li>React/Redux</li>
-    </ul>
-    <a href="/">Download Resume</a>
+    <div className={styles.storyContainer}>
+      <article className={styles.story}>
+        <p>My name is Keenan Staffieri. I am passionate about...</p>
+        <p>Velit fugiat duis sit fugiat aute velit sit enim aliquip do est duis quis.</p>
+        <p>Exercitation irure non sint velit mollit Lorem proident culpa duis labore. Id incididunt cillum laborum veniam exercitation est officia. Minim culpa voluptate consectetur labore et proident aute anim commodo laborum non ut. Occaecat adipisicing aliqua culpa tempor laborum ex cillum proident aute sint. Aliqua est deserunt anim incididunt nisi laborum amet consectetur labore fugiat mollit. Id reprehenderit exercitation id dolore velit.</p>
+        <p>Eiusmod sint ad dolore est. Irure elit officia culpa officia. Exercitation ut ullamco eiusmod laborum cillum ut incididunt. Pariatur occaecat nulla veniam est elit. Est deserunt ea officia est ea in voluptate in nostrud veniam. Elit ex ea amet ex duis enim nostrud laborum aliquip. Laborum sit eu nostrud occaecat do fugiat velit reprehenderit dolor.</p>
+        <p>Dolore irure culpa sint voluptate ullamco irure. Dolor adipisicing nisi aliquip laboris anim aliqua. Aute amet consequat sint veniam incididunt reprehenderit occaecat proident fugiat laborum. Pariatur sit officia nisi proident culpa. Reprehenderit consectetur magna qui qui consectetur laborum ut.</p>
+        <a href="/">Download Resume</a>
+      </article>
+    </div>
   </div>
 )
 
