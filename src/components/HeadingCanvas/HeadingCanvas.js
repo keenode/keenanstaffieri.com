30722dc5f6657c8c32e93218c7f5c078c91c2eb9
keenode <kstaffieri@gmail.com>
2018-07-29 16:31:56 -0700

    adding displacement clouds effect on heading canvas

--- src/components/HeadingCanvas/HeadingCanvas.js
+++ src/components/HeadingCanvas/HeadingCanvas.js
@@ -7,6 +7,9 @@ import styles from './HeadingCanvas.css'
 class HeadingCanvas extends Component {
   headingApp = null
   headingText = null
+  displacementSprite = null
+  displacementFilter = null
+  surgeTimer = 0
 
   componentDidMount() {
     const font = new FontFaceObserver('Cantarell')
@@ -44,10 +47,40 @@ class HeadingCanvas extends Component {
     })
     this.headingText = new PIXI.Text(this.props.text, style)
     this.headingText.pivot.set(-this.headingApp.renderer.width / window.devicePixelRatio + this.headingText.width, 0)
+
+    this.displacementSprite = PIXI.Sprite.fromImage('/assets/images/displacement-clouds.jpg')
+    this.displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT
+
+    this.displacementSprite.scale.x = 0.01
+    this.displacementSprite.scale.y = 0.01
+    // this.displacementSprite.x = 0
+
+    this.displacementFilter = new PIXI.filters.DisplacementFilter(this.displacementSprite)
+    this.headingApp.stage.filters = [this.displacementFilter]
+
+    this.headingApp.stage.addChild(this.displacementSprite)
     this.headingApp.stage.addChild(this.headingText)
   }
 
   renderLoop(delta) {
+    this.surgeTimer += delta
+    if (this.surgeTimer > 0) {
+      const speed = 10
+    
+      if (this.displacementSprite.x < 150) {
+        let moveAmt = speed - this.surgeTimer * 0.25
+        if (moveAmt < 0) {
+          moveAmt = 0
+        }
+        this.displacementSprite.x += (moveAmt * delta)
+        this.displacementSprite.scale.x *= 1.3
+        this.displacementSprite.scale.y *= 1.05
+        // console.log(this.displacementSprite.x)
+      } else {
+        // console.log('done')
+        this.displacementFilter.enabled = false
+      } 
+    }
   }
 
   render() {
